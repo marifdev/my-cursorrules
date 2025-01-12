9f7dd693df84d6a364addfeebb078daeebd84d00
M.Arif SARIKAYA <muhammedarif82@gmail.com>
2025-01-12 10:46:39 +0300

    show producthunt banner only in 13th

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,20 +20,25 @@ export default function RootLayout({
       <body className={inter.className}>
         <GoogleAnalytics />
         {children}
-        <a
-          href="https://www.producthunt.com/posts/mycursorrules?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-mycursorrules"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="fixed bottom-4 right-4 z-50"
-        >
-          <img
-            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=776140&theme=neutral&t=1736667490697"
-            alt="MyCursorRules - A collection of cursorrules created by developers | Product Hunt"
-            style={{ width: '250px', height: '54px' }}
-            width="250"
-            height="54"
-          />
-        </a>
+        {
+          new Date() >= new Date('2025-01-13') && new Date() < new Date('2025-01-14') && (
+            <a
+              href="https://www.producthunt.com/posts/mycursorrules?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-mycursorrules"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="fixed bottom-4 right-4 z-50"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=776140&theme=neutral&t=1736667490697"
+                alt="MyCursorRules - A collection of cursorrules created by developers | Product Hunt"
+                style={{ width: '250px', height: '54px' }}
+                width="250"
+                height="54"
+              />
+            </a>
+          )
+        }
+
       </body>
     </html>
   );
